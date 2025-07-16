d25cbde8b283a0aaec8a885c91b2a972ebcb7557
Katherine Aguirre <Kaguirre310572@gmail.com>
2025-07-16 14:49:36 -0500

    SCRIPTS

---
+++ SCRIPTS.PANTHERA/Descargar_genes.sh
@@ -0,0 +1,7 @@
+./datasets download gene gene-id 132695470 102970916 125919382 122223697 109258132 131519140 --filename Panthera_NTS.zip 
+
+./datasets download gene gene-id 102965970 122198349 132683468 109275243 125921108 131512274 --filename Panthera_VPS8.zip
+
+./datasets download gene gene-id 132692183 102966050 125923971 122217035 109248290 131507180 --filename Panthera_NEUROG2.zip
+
+./datasets download gene gene-id 131503646 102950443 122227825 109271917 132670746 125911591 --filename Panthera_TBR1.zip
